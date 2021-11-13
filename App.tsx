214aab9b65cf07aa1bed7a988618650d0db14038
SebinLee <sebinlee.artdev@gmail.com>
2021-11-14 08:24:55 +0900

    [FI-1] Initialize Firebase

--- App.tsx
+++ App.tsx
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { initializeApp } from "firebase/app";
+
+// @ts-ignore
+import {
+	FIREBASE_APIKEY,
+	FIREBASE_AUTHDOMAIN,
+	FIREBASE_PROJECTID,
+	FIREBASE_STORAGEBUCKET,
+	FIREBASE_MESSAGINGSENDERID,
+	FIREBASE_APPID,
+	FIREBASE_MEASUREMENTID,
+} from "react-native-dotenv";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	useEffect(() => {
+		// Initialize Firebase App
+		initializeApp({
+			apiKey: FIREBASE_APIKEY,
+			authDomain: FIREBASE_AUTHDOMAIN,
+			projectId: FIREBASE_PROJECTID,
+			storageBucket: FIREBASE_STORAGEBUCKET,
+			messagingSenderId: FIREBASE_MESSAGINGSENDERID,
+			appId: FIREBASE_APPID,
+			measurementId: FIREBASE_MEASUREMENTID,
+		});
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.tsx to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
