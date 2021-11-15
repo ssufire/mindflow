b22fd9cfd636ab62922855808e38cfa041d54c75
SebinLee <sebinlee.artdev@gmail.com>
2021-11-15 21:01:12 +0900

    [FI-1] Config firebase in bare-workflow react-native - Due to the security issue, Firebase Credentials are ignored.

--- App.tsx
+++ App.tsx
@@ -1,34 +1,9 @@
-import React, { useEffect } from "react";
-import { initializeApp } from "firebase/app";
+import React from "react";
 import { NativeBaseProvider } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/screen/Routes";
 
-// @ts-ignore
-import {
-	FIREBASE_APIKEY,
-	FIREBASE_AUTHDOMAIN,
-	FIREBASE_PROJECTID,
-	FIREBASE_STORAGEBUCKET,
-	FIREBASE_MESSAGINGSENDERID,
-	FIREBASE_APPID,
-	FIREBASE_MEASUREMENTID,
-} from "react-native-dotenv";
-
 export default function App() {
-	useEffect(() => {
-		// Initialize Firebase App
-		initializeApp({
-			apiKey: FIREBASE_APIKEY,
-			authDomain: FIREBASE_AUTHDOMAIN,
-			projectId: FIREBASE_PROJECTID,
-			storageBucket: FIREBASE_STORAGEBUCKET,
-			messagingSenderId: FIREBASE_MESSAGINGSENDERID,
-			appId: FIREBASE_APPID,
-			measurementId: FIREBASE_MEASUREMENTID,
-		});
-	}, []);
-
 	return (
 		<NativeBaseProvider>
 			<NavigationContainer>
--- ios/projectFire/AppDelegate.m
+++ ios/projectFire/AppDelegate.m
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -32,6 +33,8 @@ static void InitializeFlipper(UIApplication *application) {
 #if defined(FB_SONARKIT_ENABLED) && __has_include(<FlipperKit/FlipperClient.h>)
   InitializeFlipper(application);
 #endif
+  
+  [FIRApp configure];
   
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"main" initialProperties:nil];
