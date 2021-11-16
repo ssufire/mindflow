f5d8e53ab9b40fedb0d0f8b29e851c5d9d5f2b82
SebinLee <sebinlee.artdev@gmail.com>
2021-11-17 07:15:48 +0900

    Add routing functionality after user signed-in

--- App.tsx
+++ App.tsx
@@ -1,35 +1,15 @@
-import React, { useEffect } from "react";
-import { Alert, SafeAreaView, Text, View } from "react-native";
-import {
-	GoogleSignin,
-	GoogleSigninButton,
-} from "@react-native-google-signin/google-signin";
-import signInWithGoogle from "./src/lib/auth/signInWithGoogle";
-import auth from "@react-native-firebase/auth";
-import { GOOGLE_WEB_CLIENT_ID } from "@env";
-import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
 import Routes from "./src/screen/Routes";
+import { NativeBaseProvider } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
 
 const App = () => {
-	useEffect(() => {
-		GoogleSignin.configure({
-			webClientId: GOOGLE_WEB_CLIENT_ID,
-		});
-
-		auth().onAuthStateChanged((user) => {
-			if (user) {
-				console.log("Signed In", user);
-				Alert.alert("Signed In", `${JSON.stringify(user)}`);
-			} else {
-				console.log("No User Info");
-			}
-		});
-	}, []);
-
 	return (
-		<NavigationContainer>
-			<Routes />
-		</NavigationContainer>
+		<NativeBaseProvider>
+			<NavigationContainer>
+				<Routes />
+			</NavigationContainer>
+		</NativeBaseProvider>
 	);
 };
 
--- src/screen/Routes.tsx
+++ src/screen/Routes.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 import {
 	createStackNavigator,
 	StackNavigationOptions,
@@ -7,7 +8,25 @@ import {
 import Timeline from "./Timeline";
 import SignIn from "./SignIn";
 
+import auth from "@react-native-firebase/auth";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { GOOGLE_WEB_CLIENT_ID } from "@env";
+
 export default function Routes() {
+	const navigation = useNavigation();
+
+	useEffect(() => {
+		GoogleSignin.configure({
+			webClientId: GOOGLE_WEB_CLIENT_ID,
+		});
+
+		auth().onAuthStateChanged((user) => {
+			if (user) {
+				navigation.reset({ routes: [{ name: "timeline" }] });
+			}
+		});
+	}, []);
+
 	// Create StackNavigator
 	const Stack = createStackNavigator();
 
