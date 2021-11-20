18d2b5edce071e7696656d9e4554f99fd7cc2eb0
SebinLee <sebinlee.artdev@gmail.com>
2021-11-20 17:34:02 +0900

    [FI-3] Add emotionColors and adjust the order of getDummyEmotion

---
+++ src/lib/emotion/emotionColors.ts
@@ -0,0 +1,9 @@
+const emotionColors = {
+    HAPPINESS: ["#FFEBEC", "#FFD4D6", "#FAC5C8", "#FAB9BC", "#F2B6BB"],
+    SADNESS: ["#ADC9D9", "#8FB5CC", "#739CBF", "#598AB3", "#4276A6"],
+    ANGER: ["#FFA1A1", "#FF8C8C", "#E67373", "#CC5E5C", "#B34747"],
+    FINE: ["#ECFFDE", "#D8FAC0", "#C8F2AA", "#B0E68A", "#8FD96C"],
+    FEAR: ["#D9D9D9", "#A6A6A6", "#595959", "#262626", "#0D0D0D"],
+};
+
+export default emotionColors;
