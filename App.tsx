2528b213b72003d3bdf75b9b191bc51616998a8d
SebinLee <sebinlee.artdev@gmail.com>
2021-11-22 15:58:24 +0900

    Add GowunBatang(Korean SanSerif) font

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Routes from "./src/screen/Routes";
-import { NativeBaseProvider } from "native-base";
+import { extendTheme, NativeBaseProvider } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { Provider } from "react-redux";
 import store from "./src/redux/store";
 
+const theme = extendTheme({
+    fonts: {
+        heading: "GowunBatang-Regular",
+        body: "GowunBatang-Regular",
+    },
+});
+
 const App = () => {
     return (
-        <NativeBaseProvider>
+        <NativeBaseProvider theme={theme}>
             <NavigationContainer>
                 <ActionSheetProvider>
                     <Provider store={store}>
