18f828e9bad4e068293f0f323b0a071366129324
SebinLee <sebinlee.artdev@gmail.com>
2021-11-23 19:06:24 +0900

    Reflects the determinded project name, package name and bundleIdentifier

--- android/app/src/main/java/com/ssufire/mindflow/MainActivity.java
+++ android/app/src/main/java/com/ssufire/mindflow/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ssufire.projectfire;
+package com.ssufire.mindflow;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
@@ -11,7 +11,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "projectfire";
+    return "mindflow";
   }
 
   @Override
