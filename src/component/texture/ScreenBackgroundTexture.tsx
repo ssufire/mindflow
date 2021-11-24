dd153793dde98dad5da579043259d53941de01cb
SebinLee <sebinlee.artdev@gmail.com>
2021-11-24 20:14:42 +0900

    [FI-15] Reflecting the design on the login screen - Add AppleSignIn/GoogleSignIn svg files. - To render svg files, Add react-native-svg, react-native-svg-transformer.

--- src/component/texture/ScreenBackgroundTexture.tsx
+++ src/component/texture/ScreenBackgroundTexture.tsx
@@ -12,14 +12,12 @@ export default function ScreenBackgroundTexture(props) {
             right="0"
             left="0"
             top="0"
-            rounded="xl"
         >
             <FastImage
                 source={require("../../asset/backgroundPaperTexture.jpg")}
                 style={{
                     position: "absolute",
                     opacity: 0.45,
-                    borderRadius: 13,
                     bottom: 0,
                     right: 0,
                     left: 0,
--- src/screen/SignIn.tsx
+++ src/screen/SignIn.tsx
@@ -1,30 +1,56 @@
 import React from "react";
-import { Dimensions, View } from "react-native";
-import { GoogleSigninButton } from "@react-native-google-signin/google-signin";
+import { Box, Text } from "native-base";
+import {
+    Dimensions,
+    SafeAreaView,
+    StyleSheet,
+    TouchableOpacity,
+} from "react-native";
+import ScreenBackgroundTexture from "../component/texture/ScreenBackgroundTexture";
 import signInWithGoogle from "../lib/auth/signInWithGoogle";
-
-import { AppleButton } from "@invertase/react-native-apple-authentication";
 import signInWithApple from "../lib/auth/signInWithApple";
 
+//@ts-ignore
+import GoogleSignIn from "../asset/signin/GoogleSignIn.svg";
+//@ts-ignore
+import AppleSignIn from "../asset/signin/AppleSignIn.svg";
+
 export default function SignIn() {
-    const { width, height } = Dimensions.get("screen");
+    const style = StyleSheet.create({
+        buttonStyle: {
+            opacity: 0.89,
+            width: Dimensions.get("screen").width * 0.87,
+            height: Dimensions.get("screen").width * 0.17,
+        },
+    });
 
     return (
-        <View
-            style={{
-                width,
-                height,
-                alignItems: "center",
-                justifyContent: "center",
-            }}
-        >
-            <GoogleSigninButton onPress={signInWithGoogle} />
-            <AppleButton
-                buttonStyle={AppleButton.Style.WHITE}
-                buttonType={AppleButton.Type.CONTINUE}
-                style={{ width: 250, height: 45, marginTop: 30 }}
-                onPress={signInWithApple}
-            />
-        </View>
+        <ScreenBackgroundTexture>
+            <SafeAreaView style={{ flex: 1 }}>
+                <Box flex="1" py="10" px="5" justifyContent="center">
+                    <Text
+                        fontFamily="heading"
+                        fontWeight="600"
+                        fontSize="2xl"
+                        opacity="0.7"
+                    >
+                        안녕하세요
+                    </Text>
+                    <Text fontSize="xl" my="2" opacity="0.7">
+                        마음흐름을 다음의 계정으로{"\n"}시작해보세요
+                    </Text>
+                    {/* <Box flex="1" justifyContent="flex-end" alignItems="center"> */}
+                    <Box marginTop="10">
+                        <TouchableOpacity onPress={signInWithGoogle}>
+                            <GoogleSignIn style={style.buttonStyle} />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={signInWithApple}>
+                            <AppleSignIn style={style.buttonStyle} />
+                        </TouchableOpacity>
+                    </Box>
+                    {/* </Box> */}
+                </Box>
+            </SafeAreaView>
+        </ScreenBackgroundTexture>
     );
 }
