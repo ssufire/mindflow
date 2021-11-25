43a5157fafee3fd5f9c0db34a9b2b849b3a69e6d
maemenaver <maemenaver@soongsil.ac.kr>
2021-11-26 02:00:36 +0900

    🥅 [FI-12] Add emotionless case in statistics

--- src/screen/Statistics.tsx
+++ src/screen/Statistics.tsx
@@ -32,28 +32,35 @@ export default function Statistics() {
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: "#eeeeee" }}>
             <Heading>Statistics</Heading>
-
-            <Box m="3" p="3" borderRadius="lg" background="white">
-                <PeriodStatistics
-                    emotionArray={periodMajorStatistics.emotion}
-                    emotionIntensity={periodMajorStatistics.emotionIntensity}
-                />
-            </Box>
-            <Box m="3" p="3" borderRadius="lg" background="white">
-                <Text>일주일동안 이런 감정 흐름을 보였어요</Text>
-                {Object.keys(periodDailyStatistics).map((value) => {
-                    const { emotion: emotionArray, emotionIntensity } =
-                        periodDailyStatistics[value];
-
-                    return (
-                        <DailyStatistics
-                            date={value}
-                            emotionArray={emotionArray}
-                            emotionIntensity={emotionIntensity}
+            {periodMajorStatistics.emotionIntensity === -1 ? (
+                <Text>표시할 감정이 없어요!</Text>
+            ) : (
+                <>
+                    <Box m="3" p="3" borderRadius="lg" background="white">
+                        <PeriodStatistics
+                            emotionArray={periodMajorStatistics.emotion}
+                            emotionIntensity={
+                                periodMajorStatistics.emotionIntensity
+                            }
                         />
-                    );
-                })}
-            </Box>
+                    </Box>
+                    <Box m="3" p="3" borderRadius="lg" background="white">
+                        <Text>일주일동안 이런 감정 흐름을 보였어요</Text>
+                        {Object.keys(periodDailyStatistics).map((value) => {
+                            const { emotion: emotionArray, emotionIntensity } =
+                                periodDailyStatistics[value];
+
+                            return (
+                                <DailyStatistics
+                                    date={value}
+                                    emotionArray={emotionArray}
+                                    emotionIntensity={emotionIntensity}
+                                />
+                            );
+                        })}
+                    </Box>
+                </>
+            )}
         </SafeAreaView>
     );
 }
