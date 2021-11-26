cc106ba763d08f88c0bfe28ed06d8bfade71e219
SebinLee <sebinlee.artdev@gmail.com>
2021-11-27 06:04:40 +0900

    [FI-12] Get Statistics data from getSituation/Emotion/WeeklyStatistics functions

--- src/screen/Statistics.tsx
+++ src/screen/Statistics.tsx
@@ -1,55 +1,76 @@
 import React, { useEffect, useState } from "react";
-import { SafeAreaView } from "react-native";
 import { Text, Heading, ScrollView } from "native-base";
-import { useNavigation } from "@react-navigation/core";
-import getDiaryWeek from "../lib/statistics/getDiaryWeek";
+import { SafeAreaView } from "react-native";
+import { useSelector } from "react-redux";
 
 import ScreenBackgroundTexture from "../component/texture/ScreenBackgroundTexture";
 import PeriodMajorStatistics from "../component/statistics/PeriodMajorStatistics";
 import PeriodFlowStatistics from "../component/statistics/PeriodFlowStatistics";
 import SituationStatistics from "../component/statistics/SituationStatistics";
 import EmotionStatistics from "../component/statistics/EmotionStatistics";
-import { getSituationStatistics } from "../lib/statistics/getSituationStatistics";
+import getSituationStatistics from "../lib/statistics/getSituationStatistics";
+import getWeeklyStatistics from "../lib/statistics/getWeeklyStatistics";
+import { getEmotionColor } from "../lib/emotion/getEmotionColor";
+import {
+    PeriodMajorEmotionStatisticsType,
+    DailyEmotionFlowStatisticsType,
+    SituationStatisticsType,
+    EmotionStatisticsType,
+} from "../types/statistics";
+import getEmotionStatistics from "../lib/statistics/getEmotionStatistics";
 
 export default function Statistics() {
-    const navigation = useNavigation();
+    // * Get User's nickname from redux
+    const nickname = useSelector((state: any) => state.userInfo.nickname);
 
-    const [periodDailyStatistics, setPeriodDailyStatistics] = useState({});
-    const [periodMajorStatistics, setPeriodMajorStatistics] = useState({
-        emotion: [],
-        emotionIntensity: -1,
-    });
-
-    const [situation, setSituation] = useState([]);
+    const [dailyEmotionFlow, setDailyEmotionFlow] =
+        useState<DailyEmotionFlowStatisticsType>({
+            maxCount: -1,
+            emotionFlow: {},
+        });
+    const [periodMajorEmotion, setPeriodMajorEmotion] =
+        useState<PeriodMajorEmotionStatisticsType>({
+            emotion: [],
+            emotionIntensity: -1,
+        });
+    const [situation, setSituation] = useState<SituationStatisticsType[]>([]);
+    const [emotion, setEmotion] = useState<EmotionStatisticsType[]>([]);
 
     useEffect(() => {
-        // getDiaryWeek().then((res: any) => {
-        //     if (res) {
-        //         setPeriodMajorStatistics(res.periodMajorEmotion);
-        //         setPeriodDailyStatistics(res.dailyMajorEmotionFlow);
-        //         console.log("DailyEmotionFlow", res.dailyEmotionFlow);
-        //     }
-        // });
-
         getSituationStatistics().then((res) => setSituation(res));
+        getEmotionStatistics().then((res) => setEmotion(res));
+        getWeeklyStatistics().then((res) => {
+            setDailyEmotionFlow(res.dailyEmotionFlow);
+            setPeriodMajorEmotion(res.periodMajorEmotion);
+        });
     }, []);
-
-    useEffect(() => {
-        console.log("periodStatics", periodMajorStatistics);
-    }, [periodMajorStatistics]);
 
     return (
         <ScreenBackgroundTexture>
             <SafeAreaView style={{ flex: 1 }}>
                 <ScrollView px="5">
                     <Heading>Statistics</Heading>
-                    {periodMajorStatistics.emotionIntensity === -1 ? (
+                    {periodMajorEmotion.emotionIntensity === -1 ? (
                         <Text>표시할 감정이 없어요!</Text>
                     ) : (
                         <>
-                            <PeriodMajorStatistics />
-                            <PeriodFlowStatistics />
-                            <EmotionStatistics />
+                            <PeriodMajorStatistics
+                                nickname={nickname}
+                                emotionColor={periodMajorEmotion.emotion.map(
+                                    (emotion) =>
+                                        getEmotionColor(
+                                            emotion,
+                                            Math.round(
+                                                periodMajorEmotion.emotionIntensity
+                                            )
+                                        )
+                                )}
+                            />
+                            <PeriodFlowStatistics
+                                maxCount={dailyEmotionFlow.maxCount}
+                                emotionFlow={dailyEmotionFlow.emotionFlow}
+                            />
+                            <EmotionStatistics data={emotion} />
                             <SituationStatistics data={situation} />
                         </>
                     )}
